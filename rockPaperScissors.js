957feb1a50783b548b644483a546c332cf708542
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2023-07-24 19:35:38 +0200

    Coded the playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,63 @@
-function didYaWin(params) {
-    
+// Take the two choices and select a winner
+function playRound(playerChoice, compChoice) {
+    if (playerChoice === "rock") {
+        if (compChoice === "rock")
+        {
+            return "tied"
+        }
+        if (compChoice === "paper")
+        {
+            return "lose"
+        }
+        if (compChoice === "scissors")
+        {
+            return "win"
+        }
+    }
+    if (playerChoice === "paper") {
+        if (compChoice === "rock")
+        {
+            return "win"
+        }
+        if (compChoice === "paper")
+        {
+            return "tie"
+        }
+        if (compChoice === "scissors")
+        {
+            return "lose"
+        }
+    }
+    if (playerChoice === "scissors") {
+        if (compChoice === "rock")
+        {
+            return "lose"
+        }
+        if (compChoice === "paper")
+        {
+            return "win"
+        }
+        if (compChoice === "scissors")
+        {
+            return "tie"
+        }
+    }
 }
+function getCompChoice() {
+    let choices = ["rock", "paper", "scissors"];
+    let i = Math.floor(Math.random() * 3);
+    return choices[i];
+}
+
+function game() {
+    let input = prompt("What are you picking?").toLowerCase();
+    let playerChoice = input.toLowerCase();
+    let compChoice = getCompChoice(); 
+    console.log(`Computer chose ${compChoice}`)
+    let result = playRound(playerChoice, compChoice);
+    console.log(`You ${result}`);
+}
+
+game();
+
+
